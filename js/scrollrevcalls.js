a5ba3ba2acb817425498b118f30d20dcfeace7b3
rodrfreitas <r_rochadefreitas@fanshaweonline.ca>
2024-02-20 02:07:42 -0500

    Added partial version of index

--- js/scrollrevcalls.js
+++ js/scrollrevcalls.js
@@ -7,10 +7,10 @@
         delay: 400,
     })
 
-    sr.reveal(`#hero-img-large, #demo-reel, #contact-form`, {origin: 'right'})
-    sr.reveal(`#hero-heading-large, #demo-reel-card, #contact-card`, {origin: 'left'})
+    sr.reveal(`#hero-img-large, #video-player, #contact-form`, {origin: 'right'})
+    sr.reveal(`#hero-heading-large, #video-headline, #contact-card`, {origin: 'left'})
 
-    sr.reveal(`.small_sphere, .services-card, .skill-tag, .proj-sshot`, {interval: 100})
-    sr.reveal(`.skill-tag, .project-card`, {interval: 10})
+    sr.reveal(`.small_sphere, .services-card, .goals-details .proj-sshot`, {interval: 100})
+    sr.reveal(`.goals-details, .event-card, .blog-post-preview`, {interval: 10})
 
 })();
