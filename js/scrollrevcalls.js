d947485fb14ef070eda8925f78ce7a32d54beaeb
rodrfreitas <r_rochadefreitas@fanshaweonline.ca>
2024-02-28 13:16:40 -0500

    added about and get involved pages

--- js/scrollrevcalls.js
+++ js/scrollrevcalls.js
@@ -7,10 +7,10 @@
         delay: 400,
     })
 
-    sr.reveal(`#hero-img-large, #video-player, #contact-form`, {origin: 'right'})
-    sr.reveal(`#hero-heading-large, #video-headline, #contact-card, #hero-content`, {origin: 'left'})
+    sr.reveal(`#hero-img-large, #video-player, #contact-form, #about-hero-logo`, {origin: 'right'})
+    sr.reveal(`#hero-heading-large, #video-headline, #contact-card, #hero-content, #about-hero-content`, {origin: 'left'})
 
     sr.reveal(`.small_sphere, .services-card, .goals-details .proj-sshot`, {interval: 100})
-    sr.reveal(`.goals-details, .event-card, .event-card-mini, .blog-post-preview, .blog-post-preview-list`, {interval: 10})
+    sr.reveal(`.goals-details, .event-card, .event-card-mini, .blog-post-preview, .blog-post-preview-list, .founders-card`, {interval: 10})
 
 })();
